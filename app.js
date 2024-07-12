176c8e0490bf72e50e3e85170a729b8af3bb2e47
Mohammed-Parvez <mdparvezbyahatti@gmail.com>
2024-07-12 22:14:55 +0530

    Bug fix

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const listItems = document.querySelector("#list-items");
 const addBtn = document.querySelector("#addBtn");
 const error = document.querySelector("#error");
 const inputField = document.querySelector("#input-field");
-
-showData()
+showData();
 toDoList();
 taskComplete();
 
@@ -15,35 +14,36 @@ function taskComplete() {
       saveData();
     } else if (e.target.tagName === "SPAN") {
       e.target.parentElement.remove();
-      saveData()
+      saveData();
     }
   });
 }
-
 
 function toDoList() {
   addBtn.addEventListener("click", () => {
     if (inputField.value === "") {
       error.innerHTML = "You must enter something!";
-      saveData()
+      saveData();
     } else {
       error.innerHTML = "";
-      li = document.createElement("li");
-      span = document.createElement("span");
-      text = document.createTextNode(inputField.value);
+      let li = document.createElement("li");
+      let span = document.createElement("span");
+    //   let img = document.createElement("IMG");
+      let text = document.createTextNode(inputField.value);
       li.appendChild(text);
       li.appendChild(span);
+    //   li.appendChild(img);
       listItems.appendChild(li);
     }
     inputField.value = "";
-    saveData()
+    saveData();
   });
 }
 
 function saveData() {
-    localStorage.setItem('data', listItems.innerHTML)
+  localStorage.setItem("data", listItems.innerHTML);
 }
 
 function showData() {
-    listItems.innerHTML = localStorage.getItem('data')
+  listItems.innerHTML = localStorage.getItem("data");
 }
